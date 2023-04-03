5faf9a2f1eb41150b50f59812c98290ed0ae423f
randy-25 <randyrevaldo64@gmail.com>
2023-04-03 22:05:04 +0700

    add roadmap homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,12 +4,14 @@ import Carousel from "../components/Carousel";
 import Cta from "../components/Cta";
 import Judges from "../components/Judges";
 import Accordion from "../components/Accordion";
+import Roadmap from "../components/Roadmap";
 
 function HomePage() {
   return (
     <div className="">
       <Hero />
       <Carousel />
+      <Roadmap />
       <Cta />
       <Accordion />
       {/* <Judges /> */}
