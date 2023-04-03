9881f63ef02051a82164401dcb8ca26f6091977b
ceavinr <16521304@std.stei.itb.ac.id>
2023-04-03 15:24:44 +0700

    tailwind gajelas

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
       `}
     >
       <img src="/assets/icon.png" className="w-[70px]" />
-      <ul className="flex flex-grow justify-end">
+      <ul className="flex-grow justify-end hidden md:flex">
         <li className="p-4 px-10 hover:underline-offset-2 border-gray-900">
           Home
         </li>
@@ -37,13 +37,13 @@ const Navbar = () => {
           <AiOutlineDown className="pt-1" size={23}></AiOutlineDown>
         </div>
       </ul>
-      <div onClick={handleNav} className="md:hidden block z-50">
-        {!nav ? <AiOutlineClose size={25} /> : <AiOutlineMenu size={25} />}
+      <div onClick={handleNav} className="z-50 md:hidden block">
+        {nav ? <AiOutlineClose size={25} /> : <AiOutlineMenu size={25} />}
       </div>
       <div
         className={`fixed h-full border-r duration-500 z-10
           ${
-            !nav
+            nav
               ? "bg-[#F8D660] left-0 top-0 w-full border-r-gray-900 ease-in-out"
               : "bg-gradient-to-b from-[#F8D660] left-[-100%] w-3/5 top-0 ease-linear text-white "
           }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,22 +3,22 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      fontFamily:{
-        'poppins' : 'Poppins',
+      fontFamily: {
+        poppins: "Poppins",
       },
       backgroundImage: {
-        'vector11' : "url('/public/assets/Vector11.png')",
-        'vector10' : "url('/public/assets/Vector10.png')",
-        'wavePhone' : "url('/public/assets/wavePhone.png')",
-        'blobred' : "url('/public/assets/BlobblobRed.png')",
-        'blobyellow' : "url('/public/assets/BlobblobYellow.png')",
-        'radial-card' : "radial-gradient(circle at center, #FFFFFF, #FFE99E)",
-        'waveJudges' : "url('/public/assets/waveJudges.png')",
+        vector11: "url('/public/assets/Vector11.png')",
+        vector10: "url('/public/assets/Vector10.png')",
+        wavePhone: "url('/public/assets/wavePhone.png')",
+        blobred: "url('/public/assets/BlobblobRed.png')",
+        blobyellow: "url('/public/assets/BlobblobYellow.png')",
+        "radial-card": "radial-gradient(circle at center, #FFFFFF, #FFE99E)",
+        waveJudges: "url('/public/assets/waveJudges.png')",
       },
       boxShadow: {
-        'button': '5px 5px 35px 0 rgba(0, 0, 0, 0.5)',
-      }
+        button: "5px 5px 35px 0 rgba(0, 0, 0, 0.5)",
+      },
     },
   },
   plugins: [],
-}
+};
