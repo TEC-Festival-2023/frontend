7dcb05fd4f673a8944adbf7d3d5d28c531f0ee8a
ceavinrufus <18221162@std.stei.itb.ac.id>
2023-04-03 19:57:11 +0700

    navbar

--- src/config/RoutesConfig.js
+++ src/config/RoutesConfig.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import CompetitionsPage from "../pages/CompetitionsPage";
+import EventsPage from "../pages/EventsPage";
 
 const RoutesConfig = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/competitions" element={<CompetitionsPage />} />
+      <Route path="/events" element={<EventsPage />} />
     </Routes>
   );
 };
