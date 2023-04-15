d588f21fb686814b04325e0f956f534134c9f055
randy-25 <randyrevaldo64@gmail.com>
2023-04-15 16:31:14 +0700

    Benerin sedikit roadmap

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -13,7 +13,9 @@ function Roadmap() {
                             fill="none" stroke="#29375c" stroke-width="8" />
                         <foreignObject x="120" y="12" width="160" height="160" className="">
                             <button class="ml-[9px] rounded-full w-7 h-7 bg-[#353A40]  flex flex-col justify-center ">
-                                <div className="bg-white self-center rounded-full w-4 h-4"></div>
+                                <div className="basis-4 shrink-0 grow-0 self-center">
+                                    <div className="bg-white rounded-full w-4 h-4"></div>
+                                </div>
                             </button>
                             <div className="text-xs font-poppins font-bold ">Finish Line</div>
                         </foreignObject>
@@ -21,26 +23,34 @@ function Roadmap() {
                         <foreignObject x="0" y="80" width="100%" height="160" className="">
                             <div className="flex flex-row">
                                 <button class="ml-[12px] rounded-full w-7 h-7 bg-[#7EA8FF]  flex flex-col justify-center ">
-                                    <div className="bg-[#FFE99E] self-center rounded-full w-4 h-4"></div>
+                                    <div className="basis-4 shrink-0 grow-0 self-center ">
+                                        <div className="bg-[#FFE99E] rounded-full w-4 h-4"></div>
+                                    </div>
                                 </button>
                                 <div className="text-xs font-poppins font-bold pl-1 ">Grand <br /><span className="-ml-1">Summit II</span></div>
                             </div>
                         </foreignObject>
                         <foreignObject x="3" y="145" width="100%" height="160" className="">
                             <button class="ml-[50px] rounded-full w-7 h-7 bg-[#E26767]  flex flex-col justify-center ">
-                                <div className="bg-[#FFE99E] self-center rounded-full w-4 h-4"></div>
+                                <div className="basis-4 shrink-0 grow-0 self-center">
+                                    <div className="bg-[#FFE99E] rounded-full w-4 h-4"></div>
+                                </div>
                             </button>
                             <div className="text-xs font-poppins font-bold pl-3 mt-1">Business Plan <br /><span className="pl-1">Competition</span></div>
                         </foreignObject>
                         <foreignObject x="110" y="185" width="100%" height="160" className="">
                             <button class="ml-[14px] rounded-full w-7 h-7 bg-[#7EA8FF]  flex flex-col justify-center ">
-                                <div className="bg-[#FFE99E] self-center rounded-full w-4 h-4"></div>
+                                <div className="basis-4 shrink-0 grow-0 self-center">
+                                    <div className="bg-[#FFE99E] rounded-full w-4 h-4"></div>
+                                </div>
                             </button>
                             <div className="text-xs font-poppins font-bold pl-1">Grand <br /><span className="-ml-1">Summit I</span></div>
                         </foreignObject>
                         <foreignObject x="150" y="220" width="100%" height="160" className="">
                             <button class="ml-[50px] rounded-full w-7 h-7 bg-[#E26767]  flex flex-col justify-center ">
-                                <div className="bg-[#FFE99E] self-center rounded-full w-4 h-4"></div>
+                                <div className="basis-4 shrink-0 grow-0 self-center">
+                                    <div className="bg-[#FFE99E] rounded-full w-4 h-4"></div>
+                                </div>
                             </button>
                             <div className="text-xs font-poppins font-bold pl-3 mt-1">Business Case <br /><span className="pl-1">Competition</span></div>
                         </foreignObject>
@@ -48,13 +58,17 @@ function Roadmap() {
                             <div className="flex">
                                 <div className="text-xs font-poppins font-bold pl-0">Company <br /><span className="ml-3">Visit</span></div>
                                 <button class="ml-[6px] rounded-full w-7 h-7 bg-[#7EA8FF]  flex flex-col justify-center ">
-                                    <div className="bg-[#FFE99E] self-center rounded-full w-4 h-4"></div>
+                                    <div className="basis-4 shrink-0 grow-0 self-center">
+                                        <div className="bg-[#FFE99E] rounded-full w-4 h-4"></div>
+                                    </div>
                                 </button>
                             </div>
                         </foreignObject>
                         <foreignObject x="210" y="385" width="100%" height="160" className="">
                             <button class="ml-[14px] rounded-full w-7 h-7 bg-[#7EA8FF]  flex flex-col justify-center ">
-                                <div className="bg-[#FFE99E] self-center rounded-full w-4 h-4"></div>
+                                <div className="basis-4 shrink-0 grow-0 self-center">
+                                    <div className="bg-[#FFE99E] rounded-full w-4 h-4"></div>
+                                </div>
                             </button>
                             <div className="text-xs font-poppins font-bold pl-2 mt-1">Startup <br /><span className="ml-1">Expo</span></div>
                         </foreignObject>
@@ -62,30 +76,38 @@ function Roadmap() {
                             <div className="flex ">
                                 <div className="text-xs font-poppins font-bold pl-0 mt-2">YEL 3 </div>
                                 <button class="ml-[6px] rounded-full w-7 h-7 bg-[#7EA8FF]  flex flex-col justify-center ">
-                                    <div className="bg-[#FFE99E] self-center rounded-full w-4 h-4"></div>
+                                    <div className="basis-4 shrink-0 grow-0 self-center">
+                                        <div className="bg-[#FFE99E] rounded-full w-4 h-4"></div>
+                                    </div>
                                 </button>
                             </div>
                         </foreignObject>
                         <foreignObject x="95" y="520" width="100%" height="160" className="">
                             <div className="flex ">
                                 <button class="ml-[6px] rounded-full w-7 h-7 bg-[#7EA8FF]  flex flex-col justify-center ">
-                                    <div className="bg-[#FFE99E] self-center rounded-full w-4 h-4"></div>
+                                    <div className="basis-4 shrink-0 grow-0 self-center">
+                                        <div className="bg-[#FFE99E] rounded-full w-4 h-4"></div>
+                                    </div>
                                 </button>
                                 <div className="text-xs font-poppins font-bold pl-1 mt-2">YEL 2 </div>
                             </div>
                         </foreignObject>
                         <foreignObject x="130" y="580" width="100%" height="160" className="">
                             <button class="ml-[14px] rounded-full w-7 h-7 bg-[#7EA8FF]  flex flex-col justify-center ">
-                                <div className="bg-[#FFE99E] self-center rounded-full w-4 h-4"></div>
+                                <div className="basis-4 shrink-0 grow-0 self-center">
+                                    <div className="bg-[#FFE99E] rounded-full w-4 h-4"></div>
+                                </div>
                             </button>
                             <div className="text-xs font-poppins font-bold pl-2 mt-1">YEL 1</div>
                         </foreignObject>
 
                         <foreignObject x="190" y="555" width="160" height="160" className="">
                             <div className="flex flex-col">
-                                <img src={"/assets/images/Logo.png"} alt="logoTECFEST" className="w-14 pl-3" />
+                                <img src={'/assets/images/Logo.png'} alt="logoTECFEST" className="w-14 pl-3" />
                                 <button class="ml-[20px] rounded-full w-7 h-7 bg-[#353A40]  flex flex-col justify-center ">
-                                    <div className="bg-white self-center rounded-full w-4 h-4"></div>
+                                    <div className="basis-4 shrink-0 grow-0 self-center">
+                                        <div className="bg-white rounded-full w-4 h-4"></div>
+                                    </div>
                                 </button>
                                 <div className="text-xs font-poppins font-bold ml-1">Grand Opening</div>
                             </div>
@@ -104,7 +126,9 @@ function Roadmap() {
                             fill="none" stroke="#29375c" stroke-width="20" />
                         <foreignObject x="35" y="12" width="160" height="160" className="">
                             <button class="ml-[9px] rounded-full w-[10px] h-[10px] bg-[#353A40]  flex flex-col justify-center ">
-                                <div className="bg-white self-center rounded-full w-[6px] h-[6px]"></div>
+                                <div className="self-center basis-[6px] shrink-0 grow-0 ">
+                                    <div className="bg-white rounded-full w-[6px] h-[6px]"></div>
+                                </div>
                             </button>
                             <div className="text-[0.3rem] font-poppins font-bold ">Finish Line</div>
                         </foreignObject>
@@ -112,40 +136,52 @@ function Roadmap() {
                         <foreignObject x="-10" y="34" width="100%" height="160" className="">
                             <div className="flex flex-col">
                                 <button class="ml-[12px] rounded-full w-[10px] h-[10px] bg-[#7EA8FF]  flex flex-col justify-center ">
-                                    <div className="bg-[#FFE99E] self-center rounded-full w-[6px] h-[6px]"></div>
+                                    <div className="self-center basis-[6px] shrink-0 grow-0 ">
+                                        <div className="bg-[#FFE99E] rounded-full w-[6px] h-[6px]"></div>
+                                    </div>
                                 </button>
                                 <div className="text-[0.3rem] font-poppins font-bold pl-2">Grand <br /><span className="-ml-[3px]">Summit II</span></div>
                             </div>
                         </foreignObject>
                         <foreignObject x="25" y="51" width="100%" height="160" className="">
                             <button class="ml-[15px] rounded-full w-[10px] h-[10px] bg-[#E26767]  flex flex-col justify-center ">
-                                <div className="bg-[#FFE99E] self-center rounded-full w-[6px] h-[6px]"></div>
+                                <div className="self-center basis-[6px] shrink-0 grow-0 ">
+                                    <div className="bg-[#FFE99E] rounded-full w-[6px] h-[6px]"></div>
+                                </div>
                             </button>
                             <div className="text-[0.3rem] font-poppins font-bold pl-0 mt-1">Business Plan <br /><span className="pl-1">Competition</span></div>
                         </foreignObject>
                         <foreignObject x="70" y="63" width="100%" height="160" className="">
                             <button class="ml-[14px] rounded-full w-[10px] h-[10px] bg-[#7EA8FF]  flex flex-col justify-center ">
-                                <div className="bg-[#FFE99E] self-center rounded-full w-[6px] h-[6px]"></div>
+                                <div className="self-center basis-[6px] shrink-0 grow-0 ">
+                                    <div className="bg-[#FFE99E] rounded-full w-[6px] h-[6px]"></div>
+                                </div>
                             </button>
                             <div className="text-[0.3rem] font-poppins font-bold pl-3">Grand <br /><span className="-ml-1">Summit I</span></div>
                         </foreignObject>
                         <foreignObject x="110" y="75" width="100%" height="160" className="">
                             <button class="ml-[25px] rounded-full w-[10px] h-[10px] bg-[#E26767]  flex flex-col justify-center ">
-                                <div className="bg-[#FFE99E] self-center rounded-full w-[6px] h-[6px]"></div>
+                                <div className="self-center basis-[6px] shrink-0 grow-0 ">
+                                    <div className="bg-[#FFE99E] rounded-full w-[6px] h-[6px]"></div>
+                                </div>
                             </button>
                             <div className="text-[0.3rem] font-poppins font-bold pl-3 ">Business Case <br /><span className="pl-1">Competition</span></div>
                         </foreignObject>
                         <foreignObject x="140" y="102" width="100%" height="160" className="">
                             <div className="flex flex-col">
                                 <button class="ml-[6px] rounded-full w-[10px] h-[10px] bg-[#7EA8FF]  flex flex-col justify-center ">
-                                    <div className="bg-[#FFE99E] self-center rounded-full w-[6px] h-[6px]"></div>
+                                    <div className="self-center basis-[6px] shrink-0 grow-0 ">
+                                        <div className="bg-[#FFE99E] rounded-full w-[6px] h-[6px]"></div>
+                                    </div>
                                 </button>
                                 <div className="text-[0.3rem] font-poppins font-bold pl-0">Company <br /><span className="ml-[6px]">Visit</span></div>
                             </div>
                         </foreignObject>
                         <foreignObject x="90" y="115" width="100%" height="160" className="">
                             <button class="ml-[14px] rounded-full w-[10px] h-[10px] bg-[#7EA8FF]  flex flex-col justify-center ">
-                                <div className="bg-[#FFE99E] self-center rounded-full w-[6px] h-[6px]"></div>
+                                <div className="self-center basis-[6px] shrink-0 grow-0 ">
+                                    <div className="bg-[#FFE99E] rounded-full w-[6px] h-[6px]"></div>
+                                </div>
                             </button>
                             <div className="text-[0.3rem] font-poppins font-bold pl-[9px]">Startup <br /><span className="ml-1">Expo</span></div>
                         </foreignObject>
@@ -153,29 +189,38 @@ function Roadmap() {
                             <div className="flex ">
                                 <div className="text-[0.3rem] font-poppins font-bold mt-[3px]">YEL 3 </div>
                                 <button class="ml-[2px] rounded-full w-[10px] h-[10px] bg-[#7EA8FF]  flex flex-col justify-center ">
-                                    <div className="bg-[#FFE99E] self-center rounded-full w-[6px] h-[6px]"></div>
+                                    <div className="self-center basis-[6px] shrink-0 grow-0 ">
+                                        <div className="bg-[#FFE99E] rounded-full w-[6px] h-[6px]"></div>
+                                    </div>
                                 </button>
                             </div>
                         </foreignObject>
                         <foreignObject x="90" y="155" width="100%" height="160" className="">
                             <div className="flex flex-col">
                                 <button class="ml-[6px] rounded-full w-[10px] h-[10px] bg-[#7EA8FF]  flex flex-col justify-center ">
-                                    <div className="bg-[#FFE99E] self-center rounded-full w-[6px] h-[6px]"></div>
+                                    <div className="self-center basis-[6px] shrink-0 grow-0 ">
+                                        <div className="bg-[#FFE99E] rounded-full w-[6px] h-[6px]"></div>
+                                    </div>
+
                                 </button>
                                 <div className="text-[0.3rem] font-poppins font-bold pl-1 ">YEL 2 </div>
                             </div>
                         </foreignObject>
                         <foreignObject x="130" y="170" width="100%" height="160" className="">
                             <button class="ml-[14px] rounded-full w-[10px] h-[10px] bg-[#7EA8FF]  flex flex-col justify-center ">
-                                <div className="bg-[#FFE99E] self-center rounded-full w-[6px] h-[6px]"></div>
+                                <div className="self-center basis-[6px] shrink-0 grow-0 ">
+                                    <div className="bg-[#FFE99E] rounded-full w-[6px] h-[6px]"></div>
+                                </div>
                             </button>
                             <div className="text-[0.3rem] font-poppins font-bold pl-3">YEL 1</div>
                         </foreignObject>
                         <foreignObject x="176" y="161" width="160" height="160" className="">
                             <div className="flex flex-col">
-                                <img src={"/assets/images/Logo.png"} alt="logoTECFEST" className="w-7 pl-3" />
+                                <img src={'/assets/images/logo.png'} alt="logoTECFEST" className="w-7 pl-3" />
                                 <button class="ml-[14px] rounded-full w-[10px] h-[10px] bg-[#353A40]  flex flex-col justify-center ">
-                                    <div className="bg-white self-center rounded-full w-[6px] h-[6px]"></div>
+                                    <div className="self-center basis-[6px] shrink-0 grow-0">
+                                        <div className="bg-white rounded-full w-[6px] h-[6px]"></div>
+                                    </div>
                                 </button>
 
                                 <div className="text-[0.3rem] font-poppins font-bold ml-0">Grand Opening</div>
