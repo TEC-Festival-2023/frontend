917916b884667ebf1b720d987dfe8e907f70e0f2
ceavinrufus <18221162@std.stei.itb.ac.id>
2023-04-20 02:32:44 +0700

    tecfesttalks

--- src/components/HeroTecFestTalks.jsx
+++ src/components/HeroTecFestTalks.jsx
@@ -1,25 +1,30 @@
-import React from 'react'
+import React from "react";
 
 const HeroTecFestTalks = () => {
   return (
     <div>
-        <div className="p-20 font-poppins text-center">
-            <div className="text-[#FFE99E] flex justify-center text-5xl lg:text-7xl font-poppins font-bold mt-4 mb-7 min-[500px]:mt-10 min-[500px]:mb-13 min-[1000px]:mt-16 min-[1000px]:mb-10 text-shadow-3px md:text-shadow-4px ">
-            TECFEST TALKS
-            </div>
-            <h3 className=" p-3 font-bold">Presented by TECFest 2023 ITB</h3>
-            <h2 className=" p-6 text-3xl">In Collaboration With:</h2>
-            <div className="mx-auto w-40">
-                <img src='/assets/images/pembicara.png '></img>
-            </div>
-            <li>
-                ddd
-            </li>
-
+      <div className="font-poppins h-screen flex flex-col items-center justify-center -translate-y-[80px]">
+        <div className="text-[#FFE99E] flex justify-center text-4xl md:text-5xl lg:text-7xl font-poppins font-bold mt-4 text-shadow-3px md:text-shadow-5px">
+          TECFest Talks
+        </div>
+        <h3 className="mb-3 text-sm md:text-base font-bold">
+          Presented by TECFest 2023 ITB
+        </h3>
+        <h2 className="my-6 text-xl md:text-3xl">In Collaboration With:</h2>
+        <div className="flex justify-center w-40 md:w-48 lg:w-96 mb-4 md:mb-6">
+          <img src="/assets/images/pembicara.png "></img>
+        </div>
+        <h2 className="text-xl md:text-3xl lg:text-4xl font-bold">
+          Rizky Arief Dwi Prakoso
+        </h2>
+        <ol className="text-xs md:text-base lg:text-xl mt-2 text-center">
+          <li>CEO & Founder HMNS Perfumery</li>
+          <li>CEO & Co-founder NAH Project</li>
+          <li>Founding Partner in EfektifKreatif</li>
+        </ol>
       </div>
-        
     </div>
-  )
-}
+  );
+};
 
-export default HeroTecFestTalks
+export default HeroTecFestTalks;
