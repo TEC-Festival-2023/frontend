3102ed36eed00695fe45798d04663144b42c1e89
randy-25 <randyrevaldo64@gmail.com>
2023-04-20 11:36:32 +0700

    commit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,20 +1,30 @@
 import React from "react";
 import Hero from "../components/Hero";
 import Carousel from "../components/Carousel";
+import Carousel2 from "../components/Carousel2";
 import Cta from "../components/Cta";
 import Judges from "../components/Judges";
 import Accordion from "../components/Accordion";
 import Roadmap from "../components/Roadmap";
+import Miniquiz from "../components/Miniquiz";
+import HeroTECFestTalks from "../components/HeroTECFestTalks";
+import CtaTECFestTalks from "../components/CtaTECFestTalks";
+import FooterTECFestTalks from "../components/FooterTECFestTalks";
 
 function HomePage() {
   return (
     <div className="">
-      <Hero />
+      {/* <Hero />
       <Carousel />
       <Roadmap />
       <Cta />
       <Accordion />
-      {/* <Judges /> */}
+      <Carousel2 />
+      <Judges /> */}
+      <HeroTECFestTalks />
+      <Miniquiz />
+      <CtaTECFestTalks />
+      <FooterTECFestTalks />
     </div>
   );
 }
