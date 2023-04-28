1c0f126f4d663d4db2376fc850e0cf2474792398
randy-25 <randyrevaldo64@gmail.com>
2023-04-28 23:06:41 +0700

    dropdown navbar mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,12 @@
 import React, { useState } from "react";
 import { AiOutlineClose, AiOutlineMenu, AiOutlineDown } from "react-icons/ai";
 import { Dropdown, DropdownContainer } from "./Dropdown";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
@@ -53,7 +59,7 @@ const Navbar = () => {
             <a href="/competitions/business-case" className=" rounded-t-md">
               Business Case Competition
             </a>
-            <a href="/competitions/business-plan" className="rounded-t-md">
+            <a href="/competitions/business-plan" className="rounded-b-md">
               Business Plan Competition
             </a>
           </Dropdown>
@@ -64,14 +70,104 @@ const Navbar = () => {
       </div>
       <div
         className={`fixed h-full border-r duration-500 z-10
-          ${
-            nav
-              ? "bg-[#F8D660] left-0 top-0 w-full border-r-gray-900 ease-in-out"
-              : "bg-gradient-to-b from-[#F8D660] left-[-100%] w-3/5 top-0 ease-linear text-white "
+          ${nav
+            ? "bg-[#F8D660] left-0 top-0 w-full border-r-gray-900 ease-in-out"
+            : "bg-gradient-to-b from-[#F8D660] left-[-100%] w-3/5 top-0 ease-linear text-white "
           }
         `}
       >
-        <div className="flex flex-col uppercase p-4 mt-20 cursor-pointer">
+        <div className="flex flex-col uppercase py-4 mt-20 cursor-pointer">
+          <a
+            href="/"
+            className="p-8 hover:font-bold duration-100 border-b border-gray-600 text-center"
+          >
+            Home
+          </a>
+          <Accordion >
+            <div className="bg-[#F8D660] flex py-8 hover:font-bold duration-100 border-b border-gray-600">
+              <a
+                href="/events"
+                className="self-center ml-auto pl-14"
+              >
+                Events
+              </a>
+              <div className="ml-auto">
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel1a-content"
+                  id="panel1a-header"
+                  sx={{
+                    backgroundColor: "#F8D660"
+                  }}
+                >
+                  <Typography></Typography>
+                </AccordionSummary>
+              </div>
+            </div>
+            <AccordionDetails
+              sx={{
+                backgroundColor: "#F8D660"
+              }}>
+              <div className="flex flex-col">
+                <a href="/events/tecfest-talks" className="flex justify-center py-3 ">
+                  TECFest Talks
+                </a>
+                <hr className="border-slate-400" />
+                <a href="/events/youth-entrepreneur-lab" className="flex justify-center py-3 ">
+                  Youth Entrepreneur Lab
+                </a>
+                <hr className="border-slate-400" />
+                <a href="/events/startup-expo" className="flex justify-center py-3">
+                  Start-Up Expo
+                </a>
+                <hr className="border-slate-400" />
+                <a href="/events/grand-summit" className="flex justify-center pt-3 pb-2">
+                  Grand Summit
+                </a>
+              </div>
+            </AccordionDetails>
+          </Accordion>
+
+          <Accordion >
+            <div className="bg-[#F8D660] flex py-8 hover:font-bold duration-100">
+              <a
+                href="/competitions"
+                className="self-center ml-auto pl-14"
+              >
+                Competitions
+              </a>
+              <div className="ml-auto">
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel1a-content"
+                  id="panel1a-header"
+                  sx={{
+                    backgroundColor: "#F8D660"
+                  }}
+                >
+                  <Typography></Typography>
+                </AccordionSummary>
+              </div>
+            </div>
+            <AccordionDetails
+              sx={{
+                backgroundColor: "#F8D660"
+              }}>
+              <div className="flex flex-col">
+                <a href="/competitions/business-case" className="flex justify-center py-3 ">
+                  Business Case Competition
+                </a>
+                <hr className="border-slate-400" />
+                <a href="/competitions/business-plan" className="flex justify-center py-3 ">
+                  Business Plan Competition
+                </a>
+
+              </div>
+            </AccordionDetails>
+          </Accordion>
+
+          {/* 
+          
           <a
             href="/"
             className="p-8 hover:font-bold duration-100 border-b border-gray-600 text-center"
@@ -90,6 +186,7 @@ const Navbar = () => {
           >
             Competitions
           </a>
+         */}
         </div>
       </div>
     </div>
