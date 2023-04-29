c51f4698a556e6582fedef57c84db1cd06a68388
ceavinrufus <18221162@std.stei.itb.ac.id>
2023-04-29 08:24:16 +0700

    responsive, revisi dari ojan arka

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,28 +7,14 @@ import AccordionDetails from "@mui/material/AccordionDetails";
 import Typography from "@mui/material/Typography";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
-
 const Navbar = () => {
   const [nav, setNav] = useState(false);
   const handleNav = () => {
     setNav(!nav);
   };
-  // change color scroll navbar
-  const [color, setColor] = useState(false);
-  const changeColor = () => {
-    if (window.scrollY >= 96) {
-      setColor(true);
-    } else {
-      setColor(false);
-    }
-  };
-  window.addEventListener("scroll", changeColor);
-
   return (
     <div
-      className={`font-poppins z-10 w-screen flex justify-between items-center h-[80px] mx-auto px-4 text-black sticky top-0
-        ${color ? "bg-gradient-to-b from-[#F8D660] bg-opacity-60 backdrop-blur-sm" : "bg-gradient-to-b from-[#F8D660]"}
-      `}
+      className={`font-poppins z-10 w-full flex justify-between items-center h-[80px] mx-auto px-4 text-black sticky top-0 bg-gradient-to-b from-[#F8D660] bg-opacity-60 backdrop-blur-sm`}
     >
       <a href="/">
         <img src="/assets/icon.png" className="w-[70px]" />
@@ -69,26 +55,24 @@ const Navbar = () => {
         {nav ? <AiOutlineClose size={25} /> : <AiOutlineMenu size={25} />}
       </div>
       <div
-        className={`fixed h-full border-r duration-500 z-10
-          ${nav
-            ? "bg-[#F8D660] left-0 top-0 w-full border-r-gray-900 ease-in-out"
-            : "bg-gradient-to-b from-[#F8D660] left-[-100%] w-3/5 top-0 ease-linear text-white "
+        className={`fixed h-full border-r duration-500 z-10 md:hidden
+          ${
+            nav
+              ? "bg-[#F8D660] left-0 top-0 w-full border-r-gray-900 ease-in-out"
+              : "bg-gradient-to-b from-[#F8D660] left-[-100%] w-3/5 top-0 ease-linear text-white "
           }
         `}
       >
-        <div className="flex flex-col uppercase py-4 mt-20 cursor-pointer">
+        <div className="flex flex-col uppercase mt-20 cursor-pointer w-screen">
           <a
             href="/"
-            className="p-8 hover:font-bold duration-100 border-b border-gray-600 text-center"
+            className="bg-[#F8D660] p-8 hover:font-bold duration-100 border-b border-t border-gray-600 text-center"
           >
             Home
           </a>
-          <Accordion >
+          <Accordion>
             <div className="bg-[#F8D660] flex py-8 hover:font-bold duration-100 border-b border-gray-600">
-              <a
-                href="/events"
-                className="self-center ml-auto pl-14"
-              >
+              <a href="/events" className="self-center ml-auto pl-14">
                 Events
               </a>
               <div className="ml-auto">
@@ -97,7 +81,7 @@ const Navbar = () => {
                   aria-controls="panel1a-content"
                   id="panel1a-header"
                   sx={{
-                    backgroundColor: "#F8D660"
+                    backgroundColor: "#F8D660",
                   }}
                 >
                   <Typography></Typography>
@@ -106,34 +90,44 @@ const Navbar = () => {
             </div>
             <AccordionDetails
               sx={{
-                backgroundColor: "#F8D660"
-              }}>
+                backgroundColor: "#F8D660",
+              }}
+            >
               <div className="flex flex-col">
-                <a href="/events/tecfest-talks" className="flex justify-center py-3 ">
+                <a
+                  href="/events/tecfest-talks"
+                  className="flex justify-center py-3 "
+                >
                   TECFest Talks
                 </a>
                 <hr className="border-slate-400" />
-                <a href="/events/youth-entrepreneur-lab" className="flex justify-center py-3 ">
+                <a
+                  href="/events/youth-entrepreneur-lab"
+                  className="flex justify-center py-3 "
+                >
                   Youth Entrepreneur Lab
                 </a>
                 <hr className="border-slate-400" />
-                <a href="/events/startup-expo" className="flex justify-center py-3">
+                <a
+                  href="/events/startup-expo"
+                  className="flex justify-center py-3"
+                >
                   Start-Up Expo
                 </a>
                 <hr className="border-slate-400" />
-                <a href="/events/grand-summit" className="flex justify-center pt-3 pb-2">
+                <a
+                  href="/events/grand-summit"
+                  className="flex justify-center pt-3 pb-2"
+                >
                   Grand Summit
                 </a>
               </div>
             </AccordionDetails>
           </Accordion>
 
-          <Accordion >
+          <Accordion>
             <div className="bg-[#F8D660] flex py-8 hover:font-bold duration-100">
-              <a
-                href="/competitions"
-                className="self-center ml-auto pl-14"
-              >
+              <a href="/competitions" className="self-center ml-auto pl-14">
                 Competitions
               </a>
               <div className="ml-auto">
@@ -142,7 +136,7 @@ const Navbar = () => {
                   aria-controls="panel1a-content"
                   id="panel1a-header"
                   sx={{
-                    backgroundColor: "#F8D660"
+                    backgroundColor: "#F8D660",
                   }}
                 >
                   <Typography></Typography>
@@ -151,17 +145,23 @@ const Navbar = () => {
             </div>
             <AccordionDetails
               sx={{
-                backgroundColor: "#F8D660"
-              }}>
+                backgroundColor: "#F8D660",
+              }}
+            >
               <div className="flex flex-col">
-                <a href="/competitions/business-case" className="flex justify-center py-3 ">
+                <a
+                  href="/competitions/business-case"
+                  className="flex justify-center py-3 "
+                >
                   Business Case Competition
                 </a>
                 <hr className="border-slate-400" />
-                <a href="/competitions/business-plan" className="flex justify-center py-3 ">
+                <a
+                  href="/competitions/business-plan"
+                  className="flex justify-center py-3 "
+                >
                   Business Plan Competition
                 </a>
-
               </div>
             </AccordionDetails>
           </Accordion>
